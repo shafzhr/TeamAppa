1146c2d9aa4048edacf5de61bc8023353d444e0c
shafzhr <shafzhr@gmail.com>
2020-01-16 23:00:41 +0200

    Change QF class implementaion of the players data

--- quantitative_functions.py
+++ quantitative_functions.py
@@ -8,3 +8,4 @@ class QuantitativeFunctions(object):
         """
         self.ices_by_player = [ game.‎get_my_icebergs(), game.‎get_enemy_icebergs() ]        
         self.p_groups_by_player = [ game.get_my_penguin_groups(), ‎get_enemy_penguin_groups() ]
+        self.players_index = { game.get_myself(): 0, game.get_enemy(): 1 }
