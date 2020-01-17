b7a6523739e594da90ccaa65a315d57694d3b8c5
shafzhr <shafzhr@gmail.com>
2020-01-18 01:28:53 +0200

    continued implementation

--- quantitative_functions.py
+++ quantitative_functions.py
@@ -2,41 +2,42 @@ from penguin_game import *
 
 
 class QuantitativeFunctions(object):
-    def __init__(self, game):
+    def __init__(self, game, player):
         """
         :param game: the current game state
         :type game: Game
+        :type player: Player
         """
         self.game = game
+        self.player = player
         self.ices_by_player = [ game.get_my_icebergs(), game.get_enemy_icebergs() ]        
         self.p_groups_by_player = [ game.get_my_penguin_groups(), game.get_enemy_penguin_groups() ]
-        self.players_index = { game.get_myself(): 0, game.get_enemy(): 1 }
+        self.players_index { game.get_myself(): 0, game.get_enemy(): 1 }
         
     
-    def get_players_indexes(self, player):
+    def __get_players_indexes(self):
         """
         Returns the indexes of the players (given player, opposite) that are used in the properties for the lists
         ~0 == -1
         ~1 == -2
         """
-        return self.players_index[player], ~ self.players_index[player]
+        return self.players_index[self.player], ~ self.players_index[self.player]
 
 
-    def get_nearest_opposite_iceberg(self, iceberg, player):
+    def get_nearest_opposite_iceberg(self, iceberg):
         """
         Returns the nearest iceberg of the opposite team to a given iceberg
-        :type player: Player
-        :rtype: Iceberg
+        :type iceberg: Iceberg
         """
-        return sorted( self.ices_by_player[self.get_players_indexes(player)[1]], key=lambda x: x.get_turns_till_arrival(iceberg) )[0]
+        return sorted( self.ices_by_player[self.__get_players_indexes()[1]], key=lambda x: x.get_turns_till_arrival(iceberg) )[0]
 
 
-    def get_iceberg_balance(self, dest, player):
+    def get_iceberg_balance(self, dest):
         """
         Returns the balance of a specific iceberg after all the penguin groups that were sent to it by his enemy have reached it
         :type dest: Iceberg
         """
-        player_i, eny_i = self.get_players_indexes(player)
+        player_i, eny_i = self.__get_players_indexes()
         groups_per_distance = {}
         for eny_group in self.p_groups_by_player[eny_i]:
             if eny_group.destination == dest:
@@ -55,22 +56,28 @@ class QuantitativeFunctions(object):
             if turn in groups_per_distance.keys():
                 balance -= groups_per_distance[turn]
             
-            for send in self.get_my_sends_on_iceberg(dest, player):
+            for send in self.get_player_sends_on_iceberg(dest):
                 if send.turns_till_arrival == turn:
                     balance += send.penguin_amount
             
             if balance < min_balance:
                 min_balance = balance
-        return min_balance
+        return min_balance - 1
     
 
-    def get_my_sends_on_iceberg(self, dest, player):
+    def get_player_sends_on_iceberg(self, dest):
         """
         :type dest: Iceberg
-        :type player: Player
         """
-        return [ send for send in self.p_groups_by_player[self.players_index[player]] if send.destination == dest ]
+        return [ send for send in self.p_groups_by_player[self.players_index[self.player]] if send.destination == dest ]
+
+
+    def get_opposite_sends_on_iceberg(self, dest):
+        """
+        :type dest: Iceberg
+        """
+        a = dest
+        return [ send for send in self.p_groups_by_player[~self.players_index[self.player]] if send.destination == dest]
 
 
     
-                
